ce9bcce15692e892a082b8d0c629ffe1e3063a5a
ilonny <lonnyfox@bk.ru>
2019-04-20 19:02:35 +0500

    reader author es

--- views/reader/edit-author.php
+++ views/reader/edit-author.php
@@ -8,6 +8,7 @@ $form = ActiveForm::begin();
 <h1>Редактирование автора <?=$model->name?></h1>
 <?= $form->field($model, 'name')->textInput()->label('Наименование'); ?>
 <?= $form->field($model, 'name_eng')->textInput()->label('Наименование на английском'); ?>
+<?= $form->field($model, 'name_es')->textInput()->label('Наименование на испанском'); ?>
 <?//= $form->field($model, 'item_type_id')->textInput(['type' => 'hidden', 'value' => $type == 'author' ? '1' : '2'])->label(false); ?>
 <?= $form->field($model, 'description')->textInput()->label('Короткое описание (не обязательно)'); ?>
 <div class="form-group">
