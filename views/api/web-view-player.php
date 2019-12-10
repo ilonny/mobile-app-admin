dd8e93ec636055c056170abf8af11989861ead58
ilonny <lonnyfox@bk.ru>
2019-12-10 21:08:59 +0500

    html player add jquery

--- views/api/web-view-player.php
+++ views/api/web-view-player.php
@@ -24,7 +24,29 @@
         width: 100%;
         margin: 10px 0;
     }
+    .mejs__container{
+        max-width: 100%;
+        margin: 10px 0;
+    }
 </style>
 <audio controls>
     <source src="/<?= $model->file_src; ?>" type="audio/mpeg">
 </audio>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+  crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/mediaelement/4.2.12/mediaelement-and-player.min.js" integrity="sha256-z7JbZVaNbNzLvOCFHUNrjqnZRojZbRAxgr4KU2qL0qc=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mediaelement/4.2.12/mediaelementplayer.min.css" integrity="sha256-ji1bfJaTGnyscoc7LzcV9yNJy5vGKJ0frO3KJo1oaGQ=" crossorigin="anonymous" />
+  <script>
+    $(document).ready(function(){
+        $('video, audio').mediaelementplayer({
+            // Do not forget to put a final slash (/)
+            pluginPath: 'https://cdnjs.com/libraries/mediaelement/',
+            // this will allow the CDN to use Flash without restrictions
+            // (by default, this is set as `sameDomain`)
+            shimScriptAccess: 'always'
+            // more configuration
+        });
+    })
+  </script>
